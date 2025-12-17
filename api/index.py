a00a471e8dabcd14a1f5a99191b468fed3915906
shreyash0216 <shreyashatre16@gmail.com>
2025-12-17 16:54:28 +0530

    Fix Vercel configuration - remove functions/builds conflict

--- api/index.py
+++ api/index.py
@@ -1,12 +1,4 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-import sys
-import os
-
-# Add the parent directory to the path so we can import from backend
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-
-from backend.main import app
+from .main import app
 
 # This is the entry point for Vercel
 # Vercel will automatically detect this as a serverless function
